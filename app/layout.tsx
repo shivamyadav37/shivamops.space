93c3844d085c5494f29055e6a06c73609a2e51fa
Shivam Yadav <babaeminem37@gmail.com>
2026-09-16 01:04:26 +0530

    added analytics for google

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -37,7 +38,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-EC6HPZLDEM"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-EC6HPZLDEM');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
